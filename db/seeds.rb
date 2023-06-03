f70dc75a215b703abb6f72767340ce57d0d8c8c9
Cassio Santana <cassioo.roger@gmail.com>
2023-06-03 11:28:56 -0300

    build: Adds supplier, author and books creation to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'ffaker'
+
+Supplier.destroy_all
+Author.destroy_all
+
+5.times do
+  Supplier.create(name: FFaker::Name.name)
+end
+
+5.times do
+  Author.create(name: FFaker::Name.name)
+end
+
+5.times do
+  random_author_id = Author.pluck(:id).sample
+  Book.create(published_at: FFaker::Time.between(DateTime.now - 1.year, DateTime.now), author_id: random_author_id)
+end
+
