f8e630c0dfc81b5409332bf1fc0b53353588af20
Cassio Santana <cassioo.roger@gmail.com>
2023-06-03 11:55:39 -0300

    test: Add model test to supplier

--- spec/models/supplier_spec.rb
+++ spec/models/supplier_spec.rb
@@ -1,7 +1,19 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Supplier, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    let(:supplier) { build(:supplier) }
+
+    it "is valid with a name" do
+      expect(supplier).to be_valid
+    end
+
+    it "is invalid without a name" do
+      supplier.name = nil
+      expect(supplier).to be_invalid
+      expect(supplier.errors[:name]).to include("can't be blank")
+    end
+  end
 end
