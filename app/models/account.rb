a16ac3796b9a7a2364ff7cc8c6f3808160ce473b
Cassio Santana <cassioo.roger@gmail.com>
2023-06-04 17:06:05 -0300

    build: Adds validation that prevents creation of more than one account per supplier

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,5 +3,14 @@
 class Account < ApplicationRecord
   belongs_to :supplier
 
+  validate :validate_single_account
   validates :account_number, presence: true
+
+  private
+
+  def validate_single_account
+    if supplier_id && Account.exists?(supplier_id: supplier_id)
+      errors.add(:supplier, "already has an associated account")
+    end
+  end
 end
