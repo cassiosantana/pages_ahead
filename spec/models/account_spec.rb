19b939e93719c3543e1a2bdf6093b5d4384d405d
Cassio Santana <cassioo.roger@gmail.com>
2023-06-05 19:56:34 -0300

    test: Adds test invalidating missing account number

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -13,5 +13,13 @@ RSpec.describe Account, type: :model do
         expect(account).to be_valid
       end
     end
+
+    context "when account_number is NOT present" do
+      let(:account) { build(:account, supplier_id: supplier.id, account_number: nil) }
+
+      it "requires account_number to be valid" do
+        expect(account).to be_invalid
+      end
+    end
   end
 end
