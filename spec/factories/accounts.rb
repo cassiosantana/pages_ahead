5e144b1e5059f9b60ead2ad71a5947464d2ef33c
Cassio Santana <cassioo.roger@gmail.com>
2023-06-05 19:51:33 -0300

    build: Add random account number in factory account

--- spec/factories/accounts.rb
+++ spec/factories/accounts.rb
@@ -3,6 +3,6 @@
 FactoryBot.define do
   factory :account do
     supplier { nil }
-    account_number { "MyString" }
+    account_number { "#{FFaker::Random.rand(10_000..90_999)}-#{FFaker::Random.rand(0..9)}" }
   end
 end
