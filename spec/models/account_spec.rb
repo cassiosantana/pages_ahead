37d8bcc0aac9a1c457bed3a5c73f8eafe673a7d9
Cassio Santana <cassioo.roger@gmail.com>
2023-06-05 19:55:23 -0300

    test: Adds test validating presence of account number

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -3,5 +3,15 @@
 require "rails_helper"
 
 RSpec.describe Account, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    let(:supplier) { create(:supplier) }
+
+    context "when account_number is present" do
+      let(:account) { build(:account, supplier_id: supplier.id) }
+
+      it "requires account_number to be valid" do
+        expect(account).to be_valid
+      end
+    end
+  end
 end
