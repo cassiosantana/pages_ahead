7c65d0de57c37994f1e8f9a2c4df41a5bfcf7ae9
Cassio Santana <cassioo.roger@gmail.com>
2023-06-06 19:47:20 -0300

    test: when name is not present author is not created

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -4,12 +4,21 @@ require 'rails_helper'
 
 RSpec.describe Author, type: :model do
   describe "validations" do
-    let(:author) { create(:author) }
+    let(:author) { build(:author) }
 
     context "when name is present" do
 
       it "author is created" do
         expect(author).to be_valid
+      end
+    end
+
+    context "when name is not present" do
+      let(:author) { build(:author, name: nil) }
+
+      it "author is not created" do
+        expect(author).to be_invalid
+        expect(author.errors[:name]).to include("can't be blank")
       end
     end
   end
