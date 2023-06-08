c6846df419dec2a9758abf84afff284fb90637a3
Cassio Santana <cassioo.roger@gmail.com>
2023-06-08 17:36:44 -0300

    test: when publication date is present book creation is valid

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -3,5 +3,15 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    let(:author) { create(:author) }
+
+    context "when publication date is present" do
+      let(:book) { build(:book, author_id: author.id) }
+
+      it "book creation is valid" do
+        expect(book).to be_valid
+      end
+    end
+  end
 end
