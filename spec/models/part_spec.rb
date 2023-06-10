2a37a599240717b7784eb1aec0a6c86af3c7b765
Cassio Santana <cassioo.roger@gmail.com>
2023-06-10 13:39:12 -0300

    test: when part_number is NOT present parts creation is invalid

--- spec/models/part_spec.rb
+++ spec/models/part_spec.rb
@@ -5,12 +5,20 @@ require "rails_helper"
 RSpec.describe Part, type: :model do
   describe "validations" do
     let(:supplier) { create(:supplier) }
+    let(:parts) { create_list(:part, 3, supplier_id: supplier.id) }
 
     context "when part_number is present" do
-      let(:parts) { create_list(:part, 3, supplier_id: supplier.id) }
-
       it "parts creation is valid" do
         parts.each { |part| expect(part).to be_valid }
+      end
+    end
+
+    context "when part_number is NOT present" do
+      it "parts creation is invalid" do
+        parts.each do |part|
+          part.part_number = nil
+          expect(part).to be_invalid
+        end
       end
     end
   end
