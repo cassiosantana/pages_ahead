9eb4a08175533a213ae20d683b50fdeacf44738e
Cassio Santana <cassioo.roger@gmail.com>
2023-06-13 20:51:05 -0300

    style: Add line separating let from test

--- spec/models/assembly_spec.rb
+++ spec/models/assembly_spec.rb
@@ -14,6 +14,7 @@ RSpec.describe Assembly, type: :model do
 
     context "when name is not present" do
       let(:assembly) { build(:assembly, name: nil) }
+
       it "assembly creation is invalid" do
         expect(assembly).to be_invalid
       end
