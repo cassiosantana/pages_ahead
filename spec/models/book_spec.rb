7d747b4db3b7bad40e1622dd687f68b8a28af1d7
Cassio Santana <cassioo.roger@gmail.com>
2023-06-21 23:21:07 -0300

    test: when adding multiple assemblies book has all associations

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -3,11 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
+  let(:author) { create(:author) }
+  let(:book) { create(:book, author_id: author.id) }
+
   describe "validations" do
-    let(:author) { create(:author) }
 
     context "when publication date is present" do
-      let(:book) { build(:book, author_id: author.id) }
 
       it "book creation is valid" do
         expect(book).to be_valid
@@ -27,6 +28,22 @@ RSpec.describe Book, type: :model do
 
       it "book creation is invalid" do
         expect(book).to be_invalid
+      end
+    end
+  end
+
+  describe "associations with assemblies" do
+    let(:assemblies) { create_list(:assembly, 3) }
+
+    before do
+      assemblies.each do |assembly|
+        book.assemblies << assembly
+      end
+    end
+
+    context "when adding multiple assemblies" do
+      it "book has all associations" do
+        expect(book.assemblies.count).to eq(3)
       end
     end
   end
