6a73ee45113dae54677b908a6fa636b4ded61df7
Cassio Santana <cassioo.roger@gmail.com>
2023-06-30 18:27:42 -0300

    feat(supplier) add read only to account association

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -4,5 +4,7 @@ class Supplier < ApplicationRecord
   has_one :account, dependent: :destroy
   has_many :parts, dependent: :destroy
 
+  attr_readonly :account
+
   validates :name, presence: true
 end
