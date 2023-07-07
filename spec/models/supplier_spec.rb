806022c6183e3e26b1486aa2c40db33e9360117d
Cassio Santana <cassioo.roger@gmail.com>
2023-07-07 19:44:58 -0300

    test(supplier): when trying to delete a supplier with several parts supplier and your parts have been deleted

--- spec/models/supplier_spec.rb
+++ spec/models/supplier_spec.rb
@@ -41,6 +41,7 @@ RSpec.describe Supplier, type: :model do
     context "when trying to delete an supplier no associated account" do
       it "supplier has been deleted" do
         expect { supplier.destroy }.to change(Supplier, :count).by(-1)
+        expect(Supplier.exists?(supplier.id)).to be_falsey
       end
 
       it "fails to delete the supplier" do
@@ -55,6 +56,16 @@ RSpec.describe Supplier, type: :model do
 
       it "supplier and your account have been deleted" do
         expect { supplier.destroy }.to change(Supplier, :count).by(-1).and change(Account, :count).by(-1)
+      end
+    end
+
+    context "when trying to delete a supplier with several parts" do
+      let!(:parts) { create_list(:part, 3, supplier: supplier) }
+
+      it "supplier and your parts have been deleted" do
+        expect { supplier.destroy }.to change(Supplier, :count).by(-1).and change(Part, :count).by(-3)
+        expect(Supplier.exists?(supplier.id)).to be_falsey
+        expect(Part.where(id: supplier.parts.pluck(:id))).to be_empty
       end
     end
   end
