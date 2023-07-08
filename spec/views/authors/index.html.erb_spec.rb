63bc876814732c3d62043e84bd25672a895e5b1a
Cassio Santana <cassioo.roger@gmail.com>
2023-07-08 09:44:09 -0300

    test(view): add authors/index renders the list of authors

--- spec/views/authors/index.html.erb_spec.rb
+++ spec/views/authors/index.html.erb_spec.rb
@@ -17,4 +17,12 @@ RSpec.describe "authors/index", type: :view do
   it "renders the link to create a new author" do
     assert_select "a[href=?]", new_author_path, text: "New author"
   end
+
+  it "renders the list of authors" do
+    assert_select "#authors" do
+      authors.each do |author|
+        assert_select "p", text: "Author: #{author.name} | Show this author"
+      end
+    end
+  end
 end
