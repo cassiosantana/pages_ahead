1df28cb3693182d787a43924593c9fcd1ed2eb6e
Cassio Santana <cassioo.roger@gmail.com>
2023-07-08 09:09:56 -0300

    style(view): edit.html.erb_spec.rb
    Ref #65

--- spec/views/authors/edit.html.erb_spec.rb
+++ spec/views/authors/edit.html.erb_spec.rb
@@ -1,23 +1,19 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'authors/edit', type: :view do
-  let(:author) do
-    Author.create!(
-      name: 'MyString'
-    )
-  end
+RSpec.describe "authors/edit", type: :view do
+  let(:author) { create(:author) }
 
   before(:each) do
     assign(:author, author)
   end
 
-  it 'renders the edit author form' do
+  it "renders the edit author form" do
     render
 
-    assert_select 'form[action=?][method=?]', author_path(author), 'post' do
-      assert_select 'input[name=?]', 'author[name]'
+    assert_select "form[action=?][method=?]", author_path(author), "post" do
+      assert_select "input[name=?]", "author[name]"
     end
   end
 end
