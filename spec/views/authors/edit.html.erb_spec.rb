5fa3dc9fdecb5ac4a993d070937edb02aa1afe32
Cassio Santana <cassioo.roger@gmail.com>
2023-07-08 10:39:41 -0300

    test(view): refactor author edit and index tests

--- spec/views/authors/edit.html.erb_spec.rb
+++ spec/views/authors/edit.html.erb_spec.rb
@@ -7,13 +7,24 @@ RSpec.describe "authors/edit", type: :view do
 
   before(:each) do
     assign(:author, author)
+    render
   end
 
   it "renders the edit author form" do
-    render
-
-    assert_select "form[action=?][method=?]", author_path(author), "post" do
-      assert_select "input[name=?]", "author[name]"
+    expect(rendered).to have_selector("form[action='#{author_path(author)}'][method='post']") do
+      expect(rendered).to have_selector("input[name='author[name]']")
     end
+  end
+
+  it "renders the page title" do
+    expect(rendered).to have_selector("h1", text: "Editing author")
+  end
+
+  it "renders the back link" do
+    expect(rendered).to have_link("Back to authors", href: authors_path)
+  end
+
+  it "renders the show link" do
+    expect(rendered).to have_link("Show this author", href: author_path(author))
   end
 end
--- spec/views/authors/index.html.erb_spec.rb
+++ spec/views/authors/index.html.erb_spec.rb
@@ -11,17 +11,17 @@ RSpec.describe "authors/index", type: :view do
   end
 
   it "renders the page title" do
-    assert_select "h1", text: "Authors"
+    expect(rendered).to have_selector("h1", text: "Authors")
   end
 
   it "renders the link to create a new author" do
-    assert_select "a[href=?]", new_author_path, text: "New author"
+    expect(rendered).to have_link("New author", href: new_author_path)
   end
 
   it "renders the list of authors" do
-    assert_select "#authors" do
+    expect(rendered).to have_selector("#authors") do
       authors.each do |author|
-        assert_select "p", text: "Author: #{author.name} | Show this author"
+        expect(rendered).to have_selector("p", text: "Author: #{author.name} | Show this author")
       end
     end
   end
