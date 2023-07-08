25e8f8b7e24a35054be27f299127d5a2e7f689fe
Cassio Santana <cassioo.roger@gmail.com>
2023-07-08 11:26:58 -0300

    test(view): authors/show renders the button to destroy author

--- spec/views/authors/show.html.erb_spec.rb
+++ spec/views/authors/show.html.erb_spec.rb
@@ -11,22 +11,30 @@ RSpec.describe "authors/show", type: :view do
     render
   end
 
-  it "renders the author name" do
-    expect(rendered).to have_text("Name: #{author.name}")
-  end
+  context "rendering of author attributes" do
+    it "renders the author name" do
+      expect(rendered).to have_text("Name: #{author.name}")
+    end
 
-  it "renders the author's books" do
-    expect(rendered).to have_selector("p strong", text: "Books:")
-    author.books.each do |book|
-      expect(rendered).to have_selector("ul li", text: book.published_at)
+    it "renders the author's books" do
+      expect(rendered).to have_selector("p strong", text: "Books:")
+      author.books.each do |book|
+        expect(rendered).to have_selector("ul li", text: book.published_at)
+      end
     end
   end
 
-  it "renders the link to edit author" do
-    expect(rendered).to have_link("Edit this author", href: edit_author_path(author))
-  end
+  context "rendering links and button" do
+    it "renders the link to edit author" do
+      expect(rendered).to have_link("Edit this author", href: edit_author_path(author))
+    end
 
-  it "renders the link to edit author" do
-    expect(rendered).to have_link("Back to authors", href: authors_path)
+    it "renders the link to edit author" do
+      expect(rendered).to have_link("Back to authors", href: authors_path)
+    end
+
+    it "renders the button to destroy author" do
+      expect(rendered).to have_button("Destroy this author")
+    end
   end
 end
