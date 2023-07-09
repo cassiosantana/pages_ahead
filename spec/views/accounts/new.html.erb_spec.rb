46f0b72aa9480426b082c0bb401dc66ba09ade41
Cassio Santana <cassioo.roger@gmail.com>
2023-07-09 10:45:41 -0300

    test(view): accounts/new renders the page title

--- spec/views/accounts/new.html.erb_spec.rb
+++ spec/views/accounts/new.html.erb_spec.rb
@@ -16,4 +16,8 @@ RSpec.describe "accounts/new", type: :view do
       expect(rendered).to have_selector("input[name='account[account_number]']")
     end
   end
+
+  it "renders the page title" do
+    expect(rendered).to have_selector("h1", text: "New account")
+  end
 end
