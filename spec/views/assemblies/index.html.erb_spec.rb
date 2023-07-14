4af79505ebe59ae4ec93bd6b1cacb80616f48131
Cassio Santana <cassioo.roger@gmail.com>
2023-07-14 19:08:09 -0300

    test(view): assemblies/index render a list of assemblies
    Ref #74

--- spec/views/assemblies/index.html.erb_spec.rb
+++ spec/views/assemblies/index.html.erb_spec.rb
@@ -13,4 +13,12 @@ RSpec.describe "assemblies/index", type: :view do
   it "render the page title" do
     expect(rendered).to have_selector("h1", text: "Assemblies")
   end
+
+  it "render a list of assemblies" do
+    expect(rendered).to have_selector("#assemblies") do
+      assemblies.each do |assembly|
+        expect(rendered).to have_selector("p", text: "Assembly name: #{assembly.name} | Show this assembly")
+      end
+    end
+  end
 end
