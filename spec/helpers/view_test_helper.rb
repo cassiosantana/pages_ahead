455ab65b37d4e384b715fbf734c9e2369672cf35
Cassio Santana <cassioo.roger@gmail.com>
2023-07-15 11:58:24 -0300

    build: update view_test_helper.rb

--- spec/helpers/view_test_helper.rb
+++ spec/helpers/view_test_helper.rb
@@ -1,10 +1,25 @@
 module ViewTestHelper
   def expect_entity_list(entities)
-    selector = "##{entities.first.class.to_s.downcase.pluralize}"
+    selector = get_selector(entities.first)
     expect(rendered).to have_selector(selector) do
-      entities.each do
-        expect(rendered).to have_selector("p", text: entities.first.class.to_s)
+      entities.each do |entity|
+        expect(rendered).to have_selector("p", text: entity.class.to_s)
       end
     end
+    expect(rendered).to have_selector("div p", count: entities.count)
+  end
+
+  def expect_page_title(title)
+    expect(rendered).to have_selector("h1", text: title)
+  end
+
+  def expect_link_to_new(record_name)
+    expect(rendered).to have_link("New #{record_name}", href: send("new_#{record_name}_path"))
+  end
+
+  private
+
+  def get_selector(entity)
+    "##{entity.class.to_s.downcase.pluralize}"
   end
 end
