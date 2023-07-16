ea03d61c3fffacbcc0aafb83db14bf9ba1d006cb
Cassio Santana <cassioo.roger@gmail.com>
2023-07-16 10:44:56 -0300

    test(view): add assemblies/edit renders the edit assembly form test

--- spec/views/assemblies/edit.html.erb_spec.rb
+++ spec/views/assemblies/edit.html.erb_spec.rb
@@ -3,21 +3,22 @@
 require "rails_helper"
 
 RSpec.describe "assemblies/edit", type: :view do
-  let(:assembly) do
-    Assembly.create!(
-      name: "MyString"
-    )
-  end
+  let(:author) { create(:author) }
+  let(:books) { create_list(:book, 5, author: author) }
+  let(:supplier) { create(:supplier) }
+  let(:parts) { create_list(:part, 3, supplier: supplier) }
+  let(:assembly) { create(:assembly, books: books, parts: parts) }
 
   before(:each) do
     assign(:assembly, assembly)
+    render
   end
 
   it "renders the edit assembly form" do
-    render
-
-    assert_select "form[action=?][method=?]", assembly_path(assembly), "post" do
-      assert_select "input[name=?]", "assembly[name]"
+    expect(rendered).to have_selector("form[action='#{assembly_path(assembly)}'][method='post']") do
+      expect(rendered).to have_selector("input[name='assembly[name]']")
+      expect(rendered).to have_selector("input[type='checkbox'][name='assembly[part_ids][]']", count: 3)
+      expect(rendered).to have_selector("input[type='checkbox'][name='assembly[book_ids][]']", count: 5)
     end
   end
 end
