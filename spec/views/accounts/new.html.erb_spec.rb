14bb377d3890bc51b310630300bd6baed2d5b417
Cassio Santana <cassioo.roger@gmail.com>
2023-07-17 20:55:01 -0300

    refactor(test): refactor account/new test

--- spec/views/accounts/new.html.erb_spec.rb
+++ spec/views/accounts/new.html.erb_spec.rb
@@ -3,41 +3,31 @@
 require "rails_helper"
 
 RSpec.describe "accounts/new", type: :view do
-  let(:supplier) { create(:supplier) }
-  let(:account) { build(:account, supplier: supplier) }
+  let(:account) { build(:account) }
 
   before(:each) do
     assign(:account, account)
+    assign(:suppliers_without_account, [
+             Supplier.create(name: "Supplier 1"),
+             Supplier.create(name: "Supplier 2")
+           ])
     render
+  end
+
+  it "renders the page title" do
+    expect_page_title("New account")
   end
 
   it "renders new account form" do
     expect(rendered).to have_selector("form[action='#{accounts_path}'][method='post']") do
       expect(rendered).to have_selector("input[name='account[account_number]']")
+      expect(rendered).to have_selector("select[name='account[supplier_id]']")
+      expect(rendered).to have_select("account_supplier_id", with_options: ["Supplier 1", "Supplier 2"])
+      expect_submit_button("Create Account")
     end
   end
 
-  it "renders the page title" do
-    expect(rendered).to have_selector("h1", text: "New account")
-  end
-
   it "renders the back link" do
-    expect(rendered).to have_link("Back to accounts", href: accounts_path)
-  end
-
-  it "renders the create account button" do
-    expect(rendered).to have_button("Create Account")
-  end
-
-  it "displays the supplier dropdown" do
-    assign(:account, Account.new)
-    assign(:suppliers_without_account, [
-             Supplier.create(name: "Supplier 1"),
-             Supplier.create(name: "Supplier 2")
-           ])
-
-    render
-
-    expect(rendered).to have_select("account_supplier_id", with_options: ["Supplier 1", "Supplier 2"])
+    expect_link_back_to("accounts")
   end
 end
