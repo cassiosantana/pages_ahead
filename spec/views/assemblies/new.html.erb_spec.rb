87c1650af797330f979f5247ddf1fa394067d6a9
Cassio Santana <cassioo.roger@gmail.com>
2023-07-17 20:26:56 -0300

    test(view): add assemblies/new render the edit assembly form

--- spec/views/assemblies/new.html.erb_spec.rb
+++ spec/views/assemblies/new.html.erb_spec.rb
@@ -3,7 +3,11 @@
 require "rails_helper"
 
 RSpec.describe "assemblies/new", type: :view do
-  let(:assembly) { create(:assembly) }
+  let(:assembly) do
+    build(:assembly,
+          books: create_list(:book, 5, author: create(:author)),
+          parts: create_list(:part, 3, supplier: create(:supplier)))
+  end
 
   before(:each) do
     assign(:assembly, assembly)
@@ -12,5 +16,14 @@ RSpec.describe "assemblies/new", type: :view do
 
   it "render the page title" do
     expect_page_title("New assembly")
+  end
+
+  it "render the edit assembly form" do
+    expect(rendered).to have_selector("form[action='#{assemblies_path}'][method='post']") do
+      expect(rendered).to have_selector("input[name='assembly[name]']")
+      expect(rendered).to have_selector("input[type='checkbox'][name='assembly[part_ids][]']", count: 3)
+      expect(rendered).to have_selector("input[type='checkbox'][name='assembly[book_ids][]']", count: 5)
+      expect_submit_button("Create Assembly")
+    end
   end
 end
