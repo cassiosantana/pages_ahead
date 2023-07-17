fef328cfbcd04b0443d52f831d7479865bb10f64
Cassio Santana <cassioo.roger@gmail.com>
2023-07-17 19:15:47 -0300

    refactor(test): refactor authors/edit test

--- spec/views/authors/edit.html.erb_spec.rb
+++ spec/views/authors/edit.html.erb_spec.rb
@@ -10,21 +10,26 @@ RSpec.describe "authors/edit", type: :view do
     render
   end
 
-  it "renders the edit author form" do
-    expect(rendered).to have_selector("form[action='#{author_path(author)}'][method='post']") do
-      expect(rendered).to have_selector("input[name='author[name]']")
+  it "renders the page title" do
+    expect_page_title("Editing author")
+  end
+
+  context "editing the author" do
+    it "renders the edit author form" do
+      expect(rendered).to have_selector("form[action='#{author_path(author)}'][method='post']") do
+        expect(rendered).to have_selector("input[name='author[name]']")
+        expect_submit_button("Update Author")
+      end
     end
   end
 
-  it "renders the page title" do
-    expect(rendered).to have_selector("h1", text: "Editing author")
-  end
+  context "rendering links" do
+    it "renders the back link" do
+      expect_link_back_to("authors")
+    end
 
-  it "renders the back link" do
-    expect(rendered).to have_link("Back to authors", href: authors_path)
-  end
-
-  it "renders the show link" do
-    expect(rendered).to have_link("Show this author", href: author_path(author))
+    it "renders the show link" do
+      expect_link_to_show(author)
+    end
   end
 end
