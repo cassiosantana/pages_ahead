53dee50605f0b26e7c288714bc4a9f1e9456bd11
Cassio Santana <cassioo.roger@gmail.com>
2023-07-17 19:55:20 -0300

    built: update assembly factory

--- spec/factories/assemblies.rb
+++ spec/factories/assemblies.rb
@@ -3,5 +3,13 @@
 FactoryBot.define do
   factory :assembly do
     name { FFaker::Lorem.word }
+
+    after(:create) do |assembly|
+      author = create(:author)
+      supplier = create(:supplier)
+      assembly.books = create_list(:book, 5, author: author)
+      assembly.parts = create_list(:part, 3, supplier: supplier)
+    end
   end
 end
+
