e84201a0680d4a582d30a8d9d8908e53c2ded346
Cassio Santana <cassioo.roger@gmail.com>
2023-07-18 19:04:49 -0300

    test(view): Add parts/show tests
    Ref #83
    
    test(view): add parts/show render the link to part tests
    
    Ref #83
    
    test(view): add parts/show render the link to edit part tests
    
    Ref #83
    
    test(view): add parts/show render the part's assemblies tests
    
    Ref #83
    
    test(view): add parts/show render the supplier name tests
    
    Ref #83
    
    test(view): add parts/show render the part number tests
    
    Ref #83

--- spec/factories/parts.rb
+++ spec/factories/parts.rb
@@ -3,9 +3,6 @@
 FactoryBot.define do
   factory :part do
     part_number { FFaker::Random.rand(1..99_999).to_s.rjust(5, "0") }
-
-    before(:create) do |part|
-      part.supplier = create(:supplier)
-    end
+    supplier { nil }
   end
 end
--- spec/views/parts/show.html.erb_spec.rb
+++ spec/views/parts/show.html.erb_spec.rb
@@ -3,16 +3,43 @@
 require "rails_helper"
 
 RSpec.describe "parts/show", type: :view do
+  let(:supplier) { create(:supplier) }
+  let(:assemblies) { create_list(:assembly, 3) }
+  let(:part) { create(:part, supplier: supplier, assemblies: assemblies) }
+
   before(:each) do
-    assign(:part, Part.create!(
-                    part_number: "Part Number",
-                    supplier: nil
-                  ))
+    assign(:part, part)
+    render
   end
 
-  it "renders attributes in <p>" do
-    render
-    expect(rendered).to match(/Part Number/)
-    expect(rendered).to match(//)
+  context "rendering of part attributes" do
+    it "render the part number" do
+      expect(rendered).to have_text(part.part_number.to_s)
+    end
+
+    it "render the supplier name" do
+      expect(rendered).to have_text(part.supplier.name.to_s)
+    end
+
+    it "render the part's assemblies" do
+      expect(rendered).to have_selector("p strong", text: "Assemblies:")
+      part.assemblies.each do |assembly|
+        expect(rendered).to have_selector("ul li", text: assembly.name)
+      end
+    end
+  end
+
+  context "rendering links and button" do
+    it "render the link to edit part" do
+      expect_link_to_edit(part)
+    end
+
+    it "render the link to part" do
+      expect_link_back_to("parts")
+    end
+
+    it "render the button to destroy part" do
+      expect_submit_button("Destroy this part")
+    end
   end
 end
