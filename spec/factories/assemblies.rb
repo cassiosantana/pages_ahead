820aa81571b70bd9ae75062efd63c17a9302ca2a
Cassio Santana <cassioo.roger@gmail.com>
2023-07-18 20:02:02 -0300

    fix(test): fix assemblies factory
    Ref #85

--- spec/factories/assemblies.rb
+++ spec/factories/assemblies.rb
@@ -3,13 +3,5 @@
 FactoryBot.define do
   factory :assembly do
     name { FFaker::Lorem.word }
-
-    before(:create) do |assembly|
-      author = create(:author)
-      supplier = create(:supplier)
-      assembly.books = create_list(:book, 5, author: author)
-      assembly.parts = create_list(:part, 3, supplier: supplier)
-    end
   end
 end
-
