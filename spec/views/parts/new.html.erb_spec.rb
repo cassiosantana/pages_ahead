2d4cf9669af43fc1764ab619286396d5433962e0
Cassio Santana <cassioo.roger@gmail.com>
2023-07-19 19:46:30 -0300

    test(view): Add parts/new test
    Ref #82
    
    test(view): parts/new render the part form test
    
    Ref #82
    
    test(view): parts/new render the page title test
    
    Ref #82

--- spec/views/parts/new.html.erb_spec.rb
+++ spec/views/parts/new.html.erb_spec.rb
@@ -3,20 +3,33 @@
 require "rails_helper"
 
 RSpec.describe "parts/new", type: :view do
+  let(:part) { build(:part) }
   before(:each) do
-    assign(:part, Part.new(
-                    part_number: "MyString",
-                    supplier: nil
-                  ))
+    assign(:part, part)
+    assign(:suppliers, [
+             Supplier.create(name: "Supplier 1"),
+             Supplier.create(name: "Supplier 2")
+           ])
+    assign(:assemblies, create_list(:assembly, FFaker::Random.rand(0..10)))
+
+    render
   end
 
-  it "renders new part form" do
-    render
+  it "render the page title" do
+    expect_page_title("New part")
+  end
 
-    assert_select "form[action=?][method=?]", parts_path, "post" do
-      assert_select "input[name=?]", "part[part_number]"
-
-      assert_select "input[name=?]", "part[supplier_id]"
+  it "render the part form" do
+    expect(rendered).to have_selector("form[action='#{parts_path}'][method='post']") do
+      expect(rendered).to have_selector("input[name='part[part_number]']")
+      expect(rendered).to have_selector("select[name='part[supplier_id]']")
+      expect(rendered).to have_select("part_supplier_id", with_options: ["Supplier 1", "Supplier 2"])
+      expect(rendered).to have_selector("input[type='checkbox'][name='part[assembly_ids][]']", count: Assembly.count)
+      expect_submit_button("Create Part")
     end
+  end
+
+  it "render the back link" do
+    expect_link_back_to("parts")
   end
 end
