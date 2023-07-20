6f428320c2ae5f6d9c3c4262eb31b2a4a6677460
Cassio Santana <cassioo.roger@gmail.com>
2023-07-20 20:41:01 -0300

    test(view): Add books/show test
    Ref #91
    
    test(view): books/show render the link to all books test
    
    Ref #91
    
    test(view): books/show render the link to edit book test
    
    Ref #91
    
    test(view): books/show render the book's assemblies test
    
    Ref #91
    
    test(view): books/show render the author name test
    
    Ref #91
    
    test(view): books/show render the publication date test
    
    Ref #91

--- spec/views/books/show.html.erb_spec.rb
+++ spec/views/books/show.html.erb_spec.rb
@@ -3,14 +3,43 @@
 require 'rails_helper'
 
 RSpec.describe 'books/show', type: :view do
+  let(:assemblies) { create_list(:assembly, 3) }
+  let(:author) { create(:author) }
+  let(:book) { create(:book, author: author, assemblies: assemblies) }
+
   before(:each) do
-    assign(:book, Book.create!(
-                    author: nil
-                  ))
+    assign(:book, book)
+    render
   end
 
-  it 'renders attributes in <p>' do
-    render
-    expect(rendered).to match(//)
+  context "rendering of book attributes" do
+    it "render the publication date" do
+      expect(rendered).to have_text(book.published_at)
+    end
+
+    it "render the author name" do
+      expect(rendered).to have_text(book.author.name)
+    end
+
+    it "render the book's assemblies" do
+      expect(rendered).to have_selector("p strong", text: "Assemblies:")
+      book.assemblies.each do |assembly|
+        expect(rendered).to have_selector("ul li", text: assembly.name)
+      end
+    end
+  end
+
+  context "rendering links and button" do
+    it "render the link to edit book" do
+      expect_link_to_edit(book)
+    end
+
+    it "render the link to all books" do
+      expect_link_back_to("books")
+    end
+
+    it "render the button to destroy book" do
+      expect_submit_button("Destroy this book")
+    end
   end
 end
