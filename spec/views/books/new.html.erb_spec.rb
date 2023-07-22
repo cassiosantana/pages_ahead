2f9a921c30eb7ff0035ee69575344e9e235f685e
Cassio Santana <cassioo.roger@gmail.com>
2023-07-22 09:19:34 -0300

    test(view): books/new render the page title test
    Ref #90

--- spec/views/books/new.html.erb_spec.rb
+++ spec/views/books/new.html.erb_spec.rb
@@ -3,17 +3,20 @@
 require 'rails_helper'
 
 RSpec.describe 'books/new', type: :view do
+  let(:book) { build(:book) }
+
   before(:each) do
-    assign(:book, Book.new(
-                    author: nil
-                  ))
+    assign(:book, book)
+    assign(:authors, [
+             Author.create(name: "Author 1"),
+             Author.create(name: "Author 2")
+           ])
+    assign(:assemblies, create_list(:assembly, rand(11)))
+
+    render
   end
 
-  it 'renders new book form' do
-    render
-
-    assert_select 'form[action=?][method=?]', books_path, 'post' do
-      assert_select 'input[name=?]', 'book[author_id]'
-    end
+  it "render the page title" do
+    expect_page_title("New book")
   end
 end
