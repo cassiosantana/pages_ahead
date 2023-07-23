5e520e2e278a62ab11103e88756b5829f9e62886
Cassio Santana <cassioo.roger@gmail.com>
2023-07-23 09:21:03 -0300

    test(view): Add suppliers/index tests
    Ref #92
    
    test: Add render a link to new supplier test
    
    test: Add render a list of suppliers test
    
    test: Add render the page title test

--- spec/views/suppliers/index.html.erb_spec.rb
+++ spec/views/suppliers/index.html.erb_spec.rb
@@ -2,21 +2,23 @@
 
 require 'rails_helper'
 
-RSpec.describe 'suppliers/index', type: :view do
+RSpec.describe "suppliers/index", type: :view do
+  let(:suppliers) { create_list(:supplier, 3) }
+
   before(:each) do
-    assign(:suppliers, [
-             Supplier.create!(
-               name: 'Name'
-             ),
-             Supplier.create!(
-               name: 'Name'
-             )
-           ])
+    assign(:suppliers, suppliers)
+    render
   end
 
-  it 'renders a list of suppliers' do
-    render
-    cell_selector = Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
-    assert_select cell_selector, text: Regexp.new('Name'.to_s), count: 2
+  it "render the page title" do
+    expect_page_title("Suppliers")
+  end
+
+  it "render a list of suppliers" do
+    expect_object_list(suppliers)
+  end
+
+  it "render a link to new supplier" do
+    expect_link_to_new("supplier")
   end
 end
