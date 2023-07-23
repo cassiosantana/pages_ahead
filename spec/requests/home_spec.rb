19145ff0e55e97810ad911a89c5e5d630d320640
Cassio Santana <cassioo.roger@gmail.com>
2023-07-23 11:44:07 -0300

    test(view) Add Home returns http success test

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -2,10 +2,10 @@
 
 require "rails_helper"
 
-RSpec.describe "Homes", type: :request do
+RSpec.describe "Home", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/home/index"
+      get root_path
       expect(response).to have_http_status(:success)
     end
   end
