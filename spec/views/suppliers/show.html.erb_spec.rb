e2e6bb2ab5a0abe9f00181c95d14997eef300917
Cassio Santana <cassioo.roger@gmail.com>
2023-07-23 11:00:47 -0300

    test(view): Add suppliers/show tests
    test: Add render supplier name
    
    test: Add when the supplier has no associations
    
    test: Add the supplier does not have an account
    
    test: Add the parts list is empty
    
    test: Add render supplier account
    
    test: Add render supplier parts
    
    test: Add render the link to edit supplier
    
    test: Add render the link to all suppliers
    
    test: Add render the button to destroy supplier
    
    test: Add the supplier does not have an account
    
    test: Add the parts list is empty
    
    test: Add render supplier account
    
    test: Add render supplier parts
    
    test: Add render the link to edit supplier
    
    test: Add render the link to all suppliers
    
    test: Add render the button to destroy supplier
    
    Ref #95

--- spec/views/suppliers/show.html.erb_spec.rb
+++ spec/views/suppliers/show.html.erb_spec.rb
@@ -1,16 +1,132 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'suppliers/show', type: :view do
-  before(:each) do
-    assign(:supplier, Supplier.create!(
-                        name: 'Name'
-                      ))
+RSpec.describe "suppliers/show", type: :view do
+  let(:supplier) { create(:supplier) }
+  let(:parts) { create_list(:part, 3, supplier: supplier) }
+  let(:account) { create(:account, supplier: supplier) }
+  let(:supplier_without_associations) { create(:supplier) }
+
+  it "render supplier name" do
+    assign(:supplier, supplier)
+    render
+
+    expect(rendered).to have_text(supplier.name)
   end
 
-  it 'renders attributes in <p>' do
-    render
-    expect(rendered).to match(/Name/)
+
+  context "when the supplier has no associations" do
+    before(:each) do
+      assign(:supplier, supplier_without_associations)
+      render
+    end
+
+    it "the supplier does not have an account" do
+      expect(rendered).to have_selector("p strong", text: "Account:")
+      expect(supplier.account).to be_nil
+    end
+
+    it "the parts list is empty" do
+      expect(supplier.parts).to be_empty
+    end
+  end
+
+  context "when the supplier has associations" do
+    before(:each) do
+      assign(:supplier, supplier)
+      assign(:account, account)
+      assign(:parts, parts)
+
+      render
+    end
+
+    it "render supplier account" do
+      expect(rendered).to have_text(supplier.account.account_number)
+    end
+
+    it "render supplier parts" do
+      expect(supplier.parts.count).to be(3)
+      expect(supplier.parts.count).to be(Part.where(supplier: supplier).count)
+      supplier.parts.each do |part|
+        expect(rendered).to have_selector("ul li", text: part.part_number)
+      end
+    end
+  end
+
+  context "rendering links and button" do
+    before(:each) do
+      assign(:supplier, supplier)
+      render
+    end
+
+    it "render the link to edit supplier" do
+      expect_link_to_edit(supplier)
+    end
+
+    it "render the link to all suppliers" do
+      expect_link_back_to("suppliers")
+    end
+
+    it "render the button to destroy supplier" do
+      expect_submit_button("Destroy this supplier")
+    end
+  end
+
+  context "when the supplier has no associations" do
+    before(:each) do
+      assign(:supplier, supplier_without_associations)
+      render
+    end
+
+    it "the supplier does not have an account" do
+      expect(rendered).to have_selector("p strong", text: "Account:")
+      expect(supplier.account).to be_nil
+    end
+
+    it "the parts list is empty" do
+      expect(supplier.parts).to be_empty
+    end
+  end
+
+  context "when the supplier has associations" do
+    before(:each) do
+      assign(:supplier, supplier)
+      assign(:account, account)
+      assign(:parts, parts)
+
+      render
+    end
+
+    it "render supplier account" do
+      expect(rendered).to have_text(supplier.account.account_number)
+    end
+
+    it "render supplier parts" do
+      expect(supplier.parts.count).to be(3)
+      expect(supplier.parts.count).to be(Part.where(supplier: supplier).count)
+      supplier.parts.each do |part|
+        expect(rendered).to have_selector("ul li", text: part.part_number)
+      end
+    end
+  end
+
+  context "rendering links and button" do
+    before(:each) do
+      assign(:supplier, supplier)
+      render
+    end
+
+    it "render the link to edit supplier" do
+      expect_link_to_edit(supplier)
+    end
+
+    it "render the link to all suppliers" do
+      expect_link_back_to("suppliers")
+    end
+
+    it "render the button to destroy supplier" do
+      expect_submit_button("Destroy this supplier")
+    end
   end
 end
