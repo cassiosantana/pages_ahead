7c918192955cd5bb36017405f1dd347d83633b68
Cassio Santana <cassioo.roger@gmail.com>
2023-07-29 00:15:45 -0300

    style(api): Change routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  root "home#index"
   resources :assemblies
   resources :parts
   resources :accounts
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :books
   resources :authors
 
-  namespace :api, defaults:  { format: :json } do
+  namespace :api, defaults: { format: :json } do
     resources :authors
   end
 end
