2c51b5596ee45db05bf77af42da2445c73c8c7ee
Cassio Santana <cassioo.roger@gmail.com>
2023-07-29 10:40:36 -0300

    build(api): Add show action to AuthorsController
    Ref #98

--- app/controllers/api/authors_controller.rb
+++ app/controllers/api/authors_controller.rb
@@ -2,12 +2,22 @@
 
 module Api
   class AuthorsController < ApplicationController
+    before_action :set_author, only: :show
+
     def index
       @authors = Author.all
 
       respond_to do |format|
         format.json
       end
+    end
+
+    def show; end
+
+    private
+
+    def set_author
+      @author = Author.find(params[:id])
     end
   end
 end
