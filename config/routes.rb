55d538acbeca30966ab6e6cb0acfa177c58b018b
Cassio Santana <cassioo.roger@gmail.com>
2023-07-29 00:03:14 -0300

    build(api): Add author resources

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   resources :suppliers
   resources :books
   resources :authors
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api, defaults:  { format: :json } do
+    resources :authors
+  end
 end
