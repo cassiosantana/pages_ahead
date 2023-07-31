4acf0a1e7afaff9754c09c73341f7d3658b3a803
Cassio Santana <cassioo.roger@gmail.com>
2023-07-31 19:19:28 -0300

    build(api): Add suppliers index route to api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     resources :authors, only: %i[index show create update destroy]
+    resources :suppliers, only: %i[index]
   end
 end
