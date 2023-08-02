ad97c014ca4cf63126f528563866e012572c50e3
Cassio Santana <cassioo.roger@gmail.com>
2023-08-02 09:21:24 -0300

    feat(api): Add show action to Books

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -2,9 +2,22 @@
 
 module Api
   class BooksController < ApplicationController
+    before_action :set_book, only: %i[show]
 
     def index
       @books = Book.all
+    end
+
+    def show; end
+
+    private
+
+    def set_book
+      @book = Book.find_by(id: params[:id])
+
+      return if @book
+
+      render json: { message: "Book not found" }, status: :not_found
     end
   end
 end
---
+++ app/views/api/books/show.json.jbuilder
@@ -0,0 +1,2 @@
+json.id @book.id
+json.published_at @book.published_at
