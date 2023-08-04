fb8322fb9d6103d3df20eba6963b9fad856110ff
Cassio Santana <cassioo.roger@gmail.com>
2023-08-04 16:06:11 -0300

    refactor(test): Combine related assertions
    books_spec.rb

--- spec/requests/api/books_spec.rb
+++ spec/requests/api/books_spec.rb
@@ -12,16 +12,11 @@ RSpec.describe "Api::Books", type: :request do
       get "/api/books"
     end
 
-    it "returns a successful response" do
+    it "returns a successful response and correct books data" do
       expect(response).to have_http_status :ok
-    end
-
-    it "returns an array of books" do
       expect(json_response).to be_an(Array)
       expect(json_response.length).to eq(books.count)
-    end
 
-    it "returns correct book data" do
       json_response.each do |book|
         expect(book["id"]).to be_present
         expect(book["published_at"]).to be_present
@@ -39,19 +34,10 @@ RSpec.describe "Api::Books", type: :request do
 
   describe "GET /api/books/:id" do
     shared_examples "a book" do
-      it "returns a successful response" do
+      it "returns a successful response and correct book data" do
         expect(response).to have_http_status :ok
-      end
-
-      it "returns the correct book id" do
         expect(json_response["id"]).to eq(book.id)
-      end
-
-      it "returns the correct published_at" do
         expect(json_response["published_at"].to_time).to eq(book.published_at)
-      end
-
-      it "returns the correct author" do
         expect(json_response["author"]["id"]).to eq(author.id)
         expect(json_response["author"]["name"]).to eq(author.name)
       end
@@ -66,11 +52,9 @@ RSpec.describe "Api::Books", type: :request do
 
       it_behaves_like "a book"
 
-      it "returns the correct number of assemblies" do
+      it "returns the correct number and data of assemblies" do
         expect(json_response["assemblies"].length).to eq(3)
-      end
 
-      it "returns the correct assemblies" do
         assembly_ids = assemblies.map(&:id)
         assembly_names = assemblies.map(&:name)
         json_response["assemblies"].each do |assembly|
@@ -96,11 +80,8 @@ RSpec.describe "Api::Books", type: :request do
     context "when book does not exist" do
       before { get "/api/books/-1" }
 
-      it "returns a not found status" do
+      it "returns a not found status and correct error message" do
         expect(response).to have_http_status :not_found
-      end
-
-      it "returns the correct error message" do
         expect(json_response["message"]).to eq("Book not found.")
       end
     end
@@ -122,30 +103,17 @@ RSpec.describe "Api::Books", type: :request do
 
       subject { post "/api/books", params: valid_book_params }
 
-      it "increases the Book count by 1" do
+      it "creates a new book and returns correct data" do
         expect { subject }.to change(Book, :count).by(1)
-      end
 
-      it "returns a created status" do
         subject
         expect(response).to have_http_status :created
-      end
-
-      it "returns a book attributes" do
-        subject
         expect(json_response["id"]).to be_present
         expect(json_response["published_at"].to_date).to eq(valid_book_params[:book][:published_at].to_date)
-      end
-
-      it "returns the correct author" do
-        subject
         expect(json_response["author"]["id"]).to eq(author.id)
         expect(json_response["author"]["name"]).to eq(author.name)
-      end
-
-      it "returns the correct assemblies" do
-        subject
         expect(json_response["assemblies"].length).to eq(assemblies.count)
+
         assembly_ids = assemblies.map(&:id)
         assembly_names = assemblies.map(&:name)
         json_response["assemblies"].each do |assembly|
@@ -168,17 +136,11 @@ RSpec.describe "Api::Books", type: :request do
 
       subject { post "/api/books", params: invalid_book_params }
 
-      it "does not increase the Book count" do
+      it "does not create a new book and returns correct error messages" do
         expect { subject }.not_to change(Book, :count)
-      end
 
-      it "returns a unprocessable entity status" do
         subject
         expect(response).to have_http_status :unprocessable_entity
-      end
-
-      it "returns the correct error messages" do
-        subject
         expect(json_response["errors"]).to include("Published at can't be blank")
         expect(json_response["errors"]).to include("Author must exist")
       end
@@ -208,27 +170,18 @@ RSpec.describe "Api::Books", type: :request do
         patch "/api/books/#{book.id}", params: valid_book_params
       end
 
-      it "returns a successful response" do
+      it "updates the book and returns correct data" do
         expect(response).to have_http_status :ok
-      end
-
-      it "updates the published_at" do
         expect(Time.iso8601(json_response["published_at"]).change(usec: 0))
           .to eq(valid_book_params[:book][:published_at].change(usec: 0))
-      end
-
-      it "change the number of assemblies" do
         expect(json_response["assemblies"].length).to eq(8)
-      end
-
-      it "change the author" do
         expect(json_response["author"]["id"]).to eq(new_author.id)
         expect(json_response["author"]["name"]).to eq(new_author.name)
       end
     end
 
     context "when the parameters are invalid" do
-      let!(:book) { create(:book, author: author, assemblies: assemblies) }
+      let!(:book) { create(:book, author: author) }
 
       let!(:invalid_book_params) do
         {
@@ -243,11 +196,8 @@ RSpec.describe "Api::Books", type: :request do
         patch "/api/books/#{book.id}", params: invalid_book_params
       end
 
-      it "returns a unprocessable_entity status" do
+      it "does not update the book and returns correct error messages" do
         expect(response).to have_http_status :unprocessable_entity
-      end
-
-      it "returns a error messages" do
         expect(json_response["errors"]).to include("Author must exist")
         expect(json_response["errors"]).to include("Published at can't be blank")
       end
