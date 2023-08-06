77e50517e8f39fb9c9aec6cc8840cebb53ae314b
Cassio Santana <cassioo.roger@gmail.com>
2023-08-06 10:21:38 -0300

    feat(api): Add show action to Accounts

--- app/controllers/api/accounts_controller.rb
+++ app/controllers/api/accounts_controller.rb
@@ -2,8 +2,17 @@
 
 module Api
   class AccountsController < ApplicationController
+    before_action :set_account, only: %i[show]
     def index
       @accounts = Account.includes(:supplier).all
+    end
+
+    def show; end
+
+    private
+
+    def set_account
+      @account = Account.find_by(id: params[:id])
     end
   end
 end
