d9f09e3cd6d107bc0c9d25d563902561971e6507
Cassio Santana <cassioo.roger@gmail.com>
2023-08-06 10:58:30 -0300

    test(api): Add test API endpoint for showing a specific account

--- spec/requests/api/accounts_spec.rb
+++ spec/requests/api/accounts_spec.rb
@@ -14,8 +14,33 @@ RSpec.describe "Api::Accounts", type: :request do
       json_response.each do |account|
         expect(account["id"]).to be_present
         expect(account["number"]).to be_present
-        expect(account["author"]["id"]).to be_present
-        expect(account["author"]["name"]).to be_present
+        expect(account["supplier"]["id"]).to be_present
+        expect(account["supplier"]["name"]).to be_present
+      end
+    end
+  end
+
+  describe "GET /api/accounts/:id" do
+    context "when the account exists" do
+      let!(:account) { create(:account) }
+
+      it "show account data correctly" do
+        get "/api/accounts/#{account.id}"
+
+        expect(response).to have_http_status :ok
+        expect(json_response["id"]).to be_present
+        expect(json_response["number"]).to be_present
+        expect(json_response["supplier"]["id"]).to be_present
+        expect(json_response["supplier"]["name"]).to be_present
+      end
+    end
+
+    context "when the account does not exist" do
+      it "show error message" do
+        get "/api/accounts/-1"
+
+        expect(response).to have_http_status :not_found
+        expect(json_response["message"]).to eq("Account not found.")
       end
     end
   end
