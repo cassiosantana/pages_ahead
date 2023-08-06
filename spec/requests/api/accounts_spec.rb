522c32e1449882ac7eaf26d06bcca014ac733209
Cassio Santana <cassioo.roger@gmail.com>
2023-08-06 10:10:19 -0300

    test(api): Add test API endpoint for showing all accounts

--- spec/requests/api/accounts_spec.rb
+++ spec/requests/api/accounts_spec.rb
@@ -3,7 +3,20 @@
 require "rails_helper"
 
 RSpec.describe "Api::Accounts", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /api/accounts" do
+    let!(:accounts) { create_list(:account, 3) }
+
+    it "returns a list of accounts correctly" do
+      get "/api/accounts"
+
+      expect(response).to have_http_status :ok
+      expect(json_response.length).to eq(Account.all.count)
+      json_response.each do |account|
+        expect(account["id"]).to be_present
+        expect(account["number"]).to be_present
+        expect(account["author"]["id"]).to be_present
+        expect(account["author"]["name"]).to be_present
+      end
+    end
   end
 end
