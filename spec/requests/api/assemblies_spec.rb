8d227fa766d4f1a7417eef8c53ac47cfd0538ffd
Cassio Santana <cassioo.roger@gmail.com>
2023-08-08 18:32:59 -0300

    test(api): Add endpoint test list all assemblies

--- spec/requests/api/assemblies_spec.rb
+++ spec/requests/api/assemblies_spec.rb
@@ -3,7 +3,23 @@
 require "rails_helper"
 
 RSpec.describe "Api::Assemblies", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /api/assemblies" do
+    context "when request all assemblies" do
+      let!(:assemblies) { create_list(:assembly, rand(1..10)) }
+
+      it "list all assemblies" do
+        get api_assemblies_path
+
+        expect(response).to have_http_status :ok
+        expect(json_response.length).to eq(assemblies.length)
+
+        assembly_ids = assemblies.map(&:id)
+        assembly_names = assemblies.map(&:name)
+        json_response.each do |assembly|
+          expect(assembly_ids).to include(assembly["id"])
+          expect(assembly_names).to include(assembly["name"])
+        end
+      end
+    end
   end
 end
