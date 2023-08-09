d8e9da0915bf9e67f273af6e802fd353b08a6ee5
Cassio Santana <cassioo.roger@gmail.com>
2023-08-09 19:56:27 -0300

    test(api): Add endpoint test update assembly

--- spec/requests/api/assemblies_spec.rb
+++ spec/requests/api/assemblies_spec.rb
@@ -3,15 +3,50 @@
 require "rails_helper"
 
 RSpec.describe "Api::Assemblies", type: :request do
+  let!(:assemblies) { create_list(:assembly, rand(1..10)) }
+  let!(:supplier) { create(:supplier) }
+  let!(:parts) { create_list(:part, rand(1..10), supplier: supplier) }
+  let!(:books) { create_list(:book, rand(1..10)) }
+  let!(:valid_data) do
+    {
+      assembly: {
+        name: FFaker::Lorem.word,
+        book_ids: books.pluck(:id),
+        part_ids: parts.pluck(:id)
+      }
+    }
+  end
+
+  shared_examples "verify assembly creation" do
+    it "the assembly is successfully created" do
+      expect(response).to have_http_status :created
+      expect(json_response["id"]).to be_present
+      expect(json_response["name"]).to eq(valid_data[:assembly][:name])
+    end
+  end
+
+  shared_examples "verify properties in books and parts" do
+    it "returns associations correctly" do
+      expect(json_response["books"].length).to eq(books.length)
+      json_response["books"].each do |book|
+        expect(book["id"]).to be_present
+        expect(book["published_at"]).to be_present
+      end
+      expect(json_response["parts"].length).to eq(parts.length)
+      json_response["parts"].each do |part|
+        expect(part["id"]).to be_present
+        expect(part["number"]).to be_present
+      end
+    end
+  end
+
   describe "GET /api/assemblies" do
     context "when request all assemblies" do
-      let!(:assemblies) { create_list(:assembly, rand(1..10)) }
-
       it "list all assemblies" do
         get api_assemblies_path
 
         expect(response).to have_http_status :ok
-        expect(json_response.length).to eq(assemblies.length)
+        expect(json_response.length).to eq(assemblies.count)
 
         assembly_ids = assemblies.map(&:id)
         assembly_names = assemblies.map(&:name)
@@ -47,59 +82,54 @@ RSpec.describe "Api::Assemblies", type: :request do
   end
 
   describe "POST /api/assemblies" do
-    shared_examples "assembly creation with success" do
-      it "the assembly is successfully created" do
-        expect(response).to have_http_status :created
-        expect(json_response["id"]).to be_present
-        expect(json_response["name"]).to eq(valid_data[:assembly][:name])
-      end
-    end
-
     context "when trying to create an assembly without associations" do
       let!(:valid_data) { { assembly: { name: FFaker::Lorem.word } } }
 
       before { post api_assemblies_path, params: valid_data }
 
-      include_examples "assembly creation with success"
+      include_examples "verify assembly creation"
     end
 
     context "when trying to create an assembly with associations" do
-      let!(:supplier) { create(:supplier) }
-      let!(:parts) { create_list(:part, 3, supplier: supplier) }
-      let!(:books) { create_list(:book, 3) }
-      let!(:valid_data) do
-        {
-          assembly: {
-            name: FFaker::Lorem.word,
-            book_ids: books.pluck(:id),
-            part_ids: parts.pluck(:id)
-          }
-        }
-      end
-
       before { post api_assemblies_path, params: valid_data }
 
-      include_examples "assembly creation with success"
+      include_examples "verify assembly creation"
 
-      it "returns associations correctly" do
-        expect(json_response["books"].length).to eq(3)
-        json_response["books"].each do |book|
-          expect(book["id"]).to be_present
-          expect(book["published_at"]).to be_present
-        end
-        expect(json_response["parts"].length).to eq(3)
-        json_response["parts"].each do |part|
-          expect(part["id"]).to be_present
-          expect(part["number"]).to be_present
-        end
-      end
+      include_examples "verify properties in books and parts"
     end
 
-    context "when trying to create an assembly with valid data" do
+    context "when trying to create an assembly with invalid data" do
       let!(:invalid_data) { { assembly: { name: "" } } }
 
       it "the assembly is successfully created" do
         post api_assemblies_path, params: invalid_data
+
+        expect(response).to have_http_status :unprocessable_entity
+        expect(json_response["errors"]).to include("Name can't be blank")
+      end
+    end
+  end
+
+  describe "PATCH /api/assemblies/:id" do
+    context "when trying to update assembly with valid data" do
+      before do
+        patch api_assembly_path(assemblies.first), params: valid_data
+      end
+
+      it "the assembly is successful updated" do
+        expect(response).to have_http_status :ok
+        expect(json_response["id"]).to be_present
+        expect(json_response["name"]).to eq(valid_data[:assembly][:name])
+      end
+
+      include_examples "verify properties in books and parts"
+    end
+
+    context "when trying to update an assembly with invalid data" do
+      let!(:invalid_data) { { assembly: { name: "" } } }
+
+      it "the assembly is successfully created" do
+        patch api_assembly_path(assemblies.second), params: invalid_data
 
         expect(response).to have_http_status :unprocessable_entity
         expect(json_response["errors"]).to include("Name can't be blank")
