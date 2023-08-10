1f47bc2dde31972a628fcf7e67505cb2641bab79
Cassio Santana <cassioo.roger@gmail.com>
2023-08-10 18:50:21 -0300

    test(api): Add endpoint test index part

--- spec/requests/api/parts_spec.rb
+++ spec/requests/api/parts_spec.rb
@@ -3,7 +3,23 @@
 require "rails_helper"
 
 RSpec.describe "Api::Parts", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let!(:supplier) { create(:supplier) }
+  let!(:parts) { create_list(:part, 3, supplier: supplier) }
+
+  describe "GET /api/parts" do
+    context "when trying to list all parts" do
+      it "they are successfully listed" do
+        get api_parts_path
+
+        expect(response).to have_http_status :ok
+
+        part_ids = parts.pluck(:id)
+        part_numbers = parts.pluck(:part_number)
+        json_response.each do |part|
+          expect(part_ids).to include(part["id"])
+          expect(part_numbers).to include(part["number"])
+        end
+      end
+    end
   end
 end
