508594fa897322f3a0bc1ee280bcb59d7450e1ad
Cassio Santana <cassioo.roger@gmail.com>
2023-08-10 20:09:19 -0300

    test(api): Add endpoint test create part

--- spec/requests/api/parts_spec.rb
+++ spec/requests/api/parts_spec.rb
@@ -5,6 +5,7 @@ require "rails_helper"
 RSpec.describe "Api::Parts", type: :request do
   let!(:supplier) { create(:supplier) }
   let!(:parts) { create_list(:part, 3, supplier: supplier) }
+  let!(:valid_data) { { part: { part_number: rand(1000..9999).to_s, supplier_id: supplier.id } } }
 
   describe "GET /api/parts" do
     context "when trying to list all parts" do
@@ -23,7 +24,7 @@ RSpec.describe "Api::Parts", type: :request do
     end
   end
 
-  describe "GET /api/part/:id" do
+  describe "GET /api/parts/:id" do
     context "when trying to show a specific part" do
       it "it is displayed correctly" do
         part = parts.first
@@ -40,6 +41,20 @@ RSpec.describe "Api::Parts", type: :request do
 
         expect(response).to have_http_status :not_found
         expect(json_response["message"]).to eq("Part not found.")
+      end
+    end
+  end
+
+  describe "POST /api/parts" do
+    context "when trying to create an part with valid data" do
+      it "it will be created successfully" do
+        post api_parts_path, params: valid_data
+
+        expect(response).to have_http_status :created
+        expect(json_response["id"]).to be_present
+        expect(json_response["number"]).to eq(valid_data[:part][:part_number])
+        expect(json_response["supplier"]["id"]).to eq(supplier.id)
+        expect(json_response["supplier"]["name"]).to eq(supplier.name)
       end
     end
   end
