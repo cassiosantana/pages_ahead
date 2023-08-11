79c83468a219593f04bbcaa3152f6a9e3c0026c5
Cassio Santana <cassioo.roger@gmail.com>
2023-08-11 19:55:11 -0300

    feat(model): Add assembly validation to Book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,4 +5,14 @@ class Book < ApplicationRecord
   has_and_belongs_to_many :assemblies
 
   validates :published_at, presence: true
+  validate :assembly_validity, if: -> { assembly_ids.present? }
+
+  private
+
+  def assembly_validity
+    return unless Assembly.where(id: assembly_ids)
+                          .pluck(:id) == assembly_ids&.map(&:to_i) || assembly_ids.nil?
+
+    errors.add(:assembly_ids, "One or more assemblies not found.")
+  end
 end
