ab885d30d42ccbee6b752d3efb19f189380d71d3
Cassio Santana <cassioo.roger@gmail.com>
2023-08-19 22:26:20 -0300

    feat(part): Prevents supplier change

--- app/models/part.rb
+++ app/models/part.rb
@@ -7,4 +7,11 @@ class Part < ApplicationRecord
   attr_readonly :supplier_id
 
   validates :part_number, presence: true
+  validate :supplier_id_not_updated, on: :update
+
+  private
+
+  def supplier_id_not_updated
+    errors.add(:supplier_id, "cannot be updated") if supplier_id_changed?
+  end
 end
