bac61c2572a67437637882c15f70f1510a8c954c
Cassio Santana <cassioo.roger@gmail.com>
2023-08-19 23:34:59 -0300

    chore(factory): Update author creation

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -4,6 +4,6 @@ FactoryBot.define do
   factory :book do
     published_at { FFaker::Time.date }
     isbn { FFaker::Book.isbn }
-    author { Author.create(name: FFaker::Name.first_name) }
+    author { create(:author) }
   end
 end
