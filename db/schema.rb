8f10784e3fb6a0ea9a012dbb1f54e9781a4ffe3d
Cassio Santana <cassioo.roger@gmail.com>
2023-08-24 19:20:36 -0300

    feat(part): Add name
    chore(seeds): Update seeds.rb
    
    build(part): Add name to permitted params
    
    build(part): Add name to table parts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_24_141106) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_24_220323) do
   create_table "accounts", force: :cascade do |t|
     t.integer "supplier_id", null: false
     t.string "account_number"
@@ -66,6 +66,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_24_141106) do
     t.integer "supplier_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["supplier_id"], name: "index_parts_on_supplier_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,14 +12,17 @@ Book.destroy_all
 ActiveRecord::Base.connection.execute("DELETE FROM assemblies_books")
 ActiveRecord::Base.connection.execute("DELETE FROM assemblies_parts")
 
+# Author
 5.times do
   Author.create(name: FFaker::Name.name, cpf: FFaker::IdentificationBR.cpf)
 end
 
+# Supplier
 5.times do
   Supplier.create(name: FFaker::Company.name, cnpj: FFaker::IdentificationBR.cnpj)
 end
 
+# Account
 suppliers_ids = Supplier.pluck(:id)
 
 suppliers_ids.each do |supplier_id|
@@ -27,12 +30,22 @@ suppliers_ids.each do |supplier_id|
   Account.create(account_number: account_number, supplier_id: supplier_id)
 end
 
-parts = 20.times.map do
-  Part.create(part_number: rand(10_000..99_999).to_s,
-              supplier_id: Supplier.pluck(:id).sample)
+# Parts
+part_names = ["Hard Cover", "Soft Cover", "Premium Cover", "Dust Jacket", "Comic Book Cover",
+              "Standard Size Page", "Large Print Page", "Deckle Edge",
+              "Glossy Page", "Acid-free Page", "Premium Paper Page",
+              "Recycled Paper Page", "Gilded Edge", "Endpapers",
+              "Header", "Footer", "Bookmark", "Binding", "Corner Cut",
+              "Fold", "Creases"]
+
+parts = part_names.map do |name|
+  Part.create(name: name, part_number: rand(10_000..99_999), supplier_id: Supplier.pluck(:id).sample)
 end
 
-assemblies = ["paperback", "hardcover", "deluxe edition", "collector's edition", "limited edition"].map do |name|
+# Assemblies
+assembly_names = ["paperback", "hardcover", "deluxe edition", "collector's edition", "limited edition"]
+
+assemblies = assembly_names.map do |name|
   Assembly.create(name: name)
 end
 
@@ -40,6 +53,7 @@ parts.each do |part|
   part.assemblies << assemblies.sample(rand(1..5))
 end
 
+# Books
 5.times do
   random_author_id = Author.pluck(:id).sample
   book = Book.create(title: FFaker::Book.title, published_at: FFaker::Time.between(DateTime.now - 1.year, DateTime.now),
