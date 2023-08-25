c09434feee255925fe008534bf61788c63dbd092
Cassio Santana <cassioo.roger@gmail.com>
2023-08-24 21:20:12 -0300

    test(part): Update test model for name addition

--- spec/models/part_spec.rb
+++ spec/models/part_spec.rb
@@ -3,38 +3,29 @@
 require "rails_helper"
 
 RSpec.describe Part, type: :model do
-  let(:supplier) { create(:supplier) }
-  let(:parts) { build_list(:part, 3, supplier_id: supplier.id) }
   let(:assemblies) { create_list(:assembly, 5) }
-  let!(:part) { create(:part, supplier_id: supplier.id) }
 
   describe "validations" do
-
     context "when part_number is present" do
       it "parts creation is valid" do
-        parts.each { |part| expect(part).to be_valid }
+        expect(create(:part)).to be_valid
       end
     end
 
-    context "when part_number is NOT present" do
-      it "parts creation is invalid" do
-        parts.each do |part|
-          part.part_number = nil
-          expect(part).to be_invalid
-        end
-      end
-    end
+    context "when part creation is invalid" do
+      let(:part) { build(:part, part_number: nil, supplier_id: nil) }
 
-    context "when supplier is NOT present" do
-      let(:part) { build(:part, supplier_id: nil) }
-
-      it "part creation is invalid" do
+      it "it is not created and we get the error messages correctly" do
         expect(part).to be_invalid
+        expect(part.errors[:part_number]).to eq(["can't be blank"])
+        expect(part.errors[:supplier]).to eq(["must exist"])
       end
     end
   end
 
   describe "associations" do
+    let(:part) { create(:part) }
+
     before do
       assemblies.each do |assembly|
         part.assemblies << assembly
@@ -48,7 +39,6 @@ RSpec.describe Part, type: :model do
     end
 
     context "when editing associations" do
-
       it "all associations are removed" do
         part.assemblies.clear
         expect(part.assemblies.count).to eq(0)
@@ -66,28 +56,42 @@ RSpec.describe Part, type: :model do
     end
   end
 
-  describe "editing attributes" do
-    context "when editing part number" do
-      it "part number is modified" do
-        original_number = part.part_number
-        part.part_number = original_number + 1.to_s
-        part.save
+  describe "editing" do
+    let!(:part) { create(:part) }
+    context "when editing attributes with valid params" do
+      let(:valid_params) { attributes_for(:part).except(:supplier) }
 
-        expect(part.reload.part_number).not_to eq(original_number)
+      it "the part is updated" do
+        expect(part.update(valid_params)).to be true
+        valid_params.each do |attribute, value|
+          expect(part.send(attribute)).to eq(value)
+        end
+      end
+    end
+
+    context "when editing attributes with invalid params" do
+      let(:new_supplier) { create(:supplier) }
+
+      it "the part is not updated" do
+        expect(part.update(supplier: new_supplier)).to be false
+        expect(part.errors.full_messages).to eq(["Supplier cannot be updated"])
       end
     end
   end
 
   describe "destroy" do
-    context "when the part has no associated assemblies" do
+    context "when trying to delete the part that has no associated assemblies" do
+      let!(:part) { create(:part) }
+
       it "the part is deleted" do
         expect { part.destroy }.to change(Part, :count).by(-1).and change(Supplier, :count).by(0)
         expect(Part.exists?(part.id)).to be_falsey
-        expect(supplier.parts.include?(part.id)).to be_falsey
       end
     end
 
     context "when the part has associated assemblies" do
+      let!(:part) { create(:part) }
+
       it "the part is deleted and the assemblies remain intact" do
         part.assemblies << assemblies
         part.save
