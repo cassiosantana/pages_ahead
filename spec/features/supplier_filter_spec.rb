edd7128998074ee3b194afa9f0f4dbf3481cd1f4
Cassio Santana <cassioo.roger@gmail.com>
2023-09-04 16:20:02 -0300

    test(supplier): Add filter suppliers by book author name test

--- spec/features/supplier_filter_spec.rb
+++ spec/features/supplier_filter_spec.rb
@@ -5,6 +5,10 @@ require "rails_helper"
 RSpec.describe "Supplier filters", type: :feature do
   describe "Name filter" do
     let!(:supplier_one) { create(:supplier, name: "Supplier 1") }
+    let!(:part) { create(:part, supplier: supplier_one) }
+    let!(:assembly) { create(:assembly, parts: [part]) }
+    let!(:book) { create(:book, assemblies: [assembly]) }
+
     let!(:supplier_two) { create(:supplier, name: "Supplier 2") }
     let!(:supplier_three) { create(:supplier, name: "Supplier 3") }
     let!(:account) { create(:account) }
@@ -27,7 +31,7 @@ RSpec.describe "Supplier filters", type: :feature do
 
     context "when filtering by name" do
       before do
-        fill_in "q_name_or_account_account_number_cont", with: "Supplier 1"
+        fill_in "q_name_or_account_account_number_or_parts_assemblies_books_author_name_cont", with: "Supplier 1"
         click_button "Search"
       end
 
@@ -37,9 +41,9 @@ RSpec.describe "Supplier filters", type: :feature do
         expect(page).to have_no_content(supplier_three.name)
       end
 
-      context "when removing the name filter" do
+      context "when removing parameter filter" do
         before do
-          fill_in "q_name_or_account_account_number_cont", with: ""
+          fill_in "q_name_or_account_account_number_or_parts_assemblies_books_author_name_cont", with: ""
           click_button "Search"
         end
 
@@ -49,13 +53,28 @@ RSpec.describe "Supplier filters", type: :feature do
 
     context "when filtering by account number" do
       before do
-        fill_in "q_name_or_account_account_number_cont", with: account.account_number
+        fill_in "q_name_or_account_account_number_or_parts_assemblies_books_author_name_cont",
+                with: account.account_number
         click_button "Search"
       end
 
       it "filters suppliers correctly" do
         expect(page).to have_content(account.supplier.name)
         expect(page).to have_no_content(supplier_one.name)
+        expect(page).to have_no_content(supplier_two.name)
+        expect(page).to have_no_content(supplier_three.name)
+      end
+    end
+
+    context "when filtering by author name" do
+      before do
+        fill_in "q_name_or_account_account_number_or_parts_assemblies_books_author_name_cont",
+                with: book.author.name
+        click_button "Search"
+      end
+
+      it "filters suppliers correctly" do
+        expect(page).to have_content(supplier_one.name)
         expect(page).to have_no_content(supplier_two.name)
         expect(page).to have_no_content(supplier_three.name)
       end
