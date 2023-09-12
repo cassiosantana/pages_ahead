4988caa63ee3840b7dbf6e0f722393d575fb1436
Cassio Santana <cassioo.roger@gmail.com>
2023-09-12 00:05:21 -0300

    chore(view): Update homepage

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -8,7 +8,6 @@ RSpec.describe "home/index.html.erb", type: :view do
       render
 
       expect(rendered).to have_selector("h1", text: "Welcome to Pages Ahead")
-      expect(rendered).to have_selector("p", text: "Pages Ahead is an application that allows you to perform CRUD operations. With this application, you can register Authors, create Accounts, add Assemblies, Parts, Books, and Suppliers. Additionally, you can associate Accounts with Authors, Assemblies with Parts and Books, and Suppliers with Parts.")
     end
   end
 end
