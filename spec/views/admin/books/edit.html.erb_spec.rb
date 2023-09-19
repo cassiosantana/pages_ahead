12ea84d5b93109d2572db2b9c00a446f08d927ac
Cassio Santana <cassioo.roger@gmail.com>
2023-09-18 22:42:07 -0300

    test(book): Refactor tests

--- spec/views/admin/books/edit.html.erb_spec.rb
+++ spec/views/admin/books/edit.html.erb_spec.rb
@@ -2,7 +2,7 @@
 
 require "rails_helper"
 
-RSpec.describe "books/edit", type: :view do
+RSpec.describe "admin/books/edit", type: :view do
   let(:authors) { create_list(:author, 3) }
   let(:not_associated_assemblies) { create_list(:assembly, 3) }
   let(:associated_assemblies) { create_list(:assembly, 4) }
@@ -16,11 +16,11 @@ RSpec.describe "books/edit", type: :view do
   end
 
   it "render the page title" do
-    expect_page_title("Editing book")
+    expect(rendered).to have_selector("h1", text: "Editing book")
   end
 
   it "renders the book form" do
-    form = "form[action='#{book_path(book)}'][method='post']"
+    form = "form[action='#{admin_book_path(book)}'][method='post']"
     title = "input[type='text'][name='book[title]'][value='#{book.title}']"
     datetime = "input[type='datetime-local'][name='book[published_at]']"
     isbn = "input[type='text'][name='book[isbn]'][value='#{book.isbn}']"
@@ -44,14 +44,14 @@ RSpec.describe "books/edit", type: :view do
     datetime_input_value = Capybara.string(rendered).find(datetime).value
     expect(DateTime.parse(datetime_input_value).utc.iso8601).to eq(book.published_at.utc.iso8601)
 
-    expect_submit_button("Update Book")
+    expect(rendered).to have_button("Update Book")
   end
 
   it "render the show book link" do
-    expect_link_to_show(book)
+    expect(rendered).to have_link("Show this book", href: admin_book_path(book))
   end
 
   it "render the back link" do
-    expect_link_back_to("books")
+    expect(rendered).to have_link("Back to books", href: admin_books_path)
   end
 end
